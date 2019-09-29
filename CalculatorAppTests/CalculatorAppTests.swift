1f54d9301db0b8e26ee5b0558838de42c232cc5e
aviciida <ryoiida9507@gmail.com>
2019-09-29 12:58:51 +0900

    completed test for pop function

---
+++ CalculatorAppTests/CalculatorAppTests.swift
@@ -0,0 +1,32 @@
+//
+//  CalculatorAppTests.swift
+//  CalculatorAppTests
+//
+//  Created by 飯田諒 on 2019/09/29.
+//  Copyright © 2019 RyoIida. All rights reserved.
+//
+
+import XCTest
+@testable import CalculatorApp
+
+class CalculatorAppTests: XCTestCase {
+
+    func testCalculationsPopNumberOpen() {
+        let op = Calculations.popNumber(isOpen: true,
+                                        prevNumber: "234",
+                                        inputNumber: "55")
+        XCTAssertEqual(op.displayNumber, "23")
+        XCTAssertEqual(op.inputNumber, "55")
+        XCTAssertEqual(op.prevNumber, "23")
+    }
+    
+    func testCalculationsPopNumberNotOpen() {
+        let op = Calculations.popNumber(isOpen: false,
+                                        prevNumber: "234",
+                                        inputNumber: "55")
+        XCTAssertEqual(op.displayNumber, "5")
+        XCTAssertEqual(op.inputNumber, "5")
+        XCTAssertEqual(op.prevNumber, "234")
+    }
+
+}
