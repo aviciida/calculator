b860652288aaadf133a39bbbab88cd1c52d38a7b
aviciida <ryoiida9507@gmail.com>
2019-10-04 14:17:15 +0900

    :construction: Add TODO in canAppendTo in CalculationInput

--- CalculatorApp/Model/CalculationInput.swift
+++ CalculatorApp/Model/CalculationInput.swift
@@ -9,5 +9,21 @@
 import Foundation
 
 enum CalculationInput {
+    case dot
+    case `operator`
+    case number
     
+    init?(text: String?) {
+        guard let inputText = text else { return nil }
+        
+        
+        
+    }
+    
+    func canAppendTo(rawText: String) -> Bool {
+        // TODO: implement here.
+        // inputのタイプによって、rawTextの最後が何の場合はokで何の場合はngかあるので、それでswitch してケース分けする（ex: .dotの場合は、演算子/ドットじゃなければok）
+
+        return false
+    }
 }
